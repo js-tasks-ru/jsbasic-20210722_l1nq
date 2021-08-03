09cef919a033c9ea25ad8cde2a87c7cb17ed1a8b
Artem Miroshnichenko <aimiroshnichenko@edu.hse.ru>
2021-08-04 00:40:42 +0300

    Усечение строки - решено

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -1,3 +1,9 @@
 function truncate(str, maxlength) {
-  // ваш код...
+  let newStr;
+  if (str.length > maxlength - 1) {
+    newStr = str.slice(0, maxlength - 1) + "…";
+  } else {
+    newStr = str;
+  }
+  return newStr;
 }
