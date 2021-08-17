4b28c1b744e7caca7a6a0353849e0ea987f2b7fb
Artem Miroshnichenko <aimiroshnichenko@edu.hse.ru>
2021-08-17 19:23:09 +0300

    Скрыть элемент по нажатию кнопки - решена(маленько исправил)

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,6 +1,5 @@
 function toggleText() {
   document.querySelector("button.toggle-text-button").onclick = function() { 
     document.getElementById("text").hidden = !document.getElementById("text").hidden;
-  }
-  
+  };
 }
