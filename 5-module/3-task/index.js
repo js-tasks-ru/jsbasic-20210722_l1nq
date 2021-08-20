20514f34dbc504e86bcd59ee6a17dbdfa8fb1926
Artem Miroshnichenko <aimiroshnichenko@edu.hse.ru>
2021-08-20 12:57:36 +0300

    Таблица с удаляемыми строками - решена

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,13 +1,12 @@
 function initCarousel() {
   document.querySelector(".carousel__arrow_left").style.display = "none";
   let trnslt = 0;
-  document.addEventListener("click", (event)=> {
+  document.querySelector("div.container").addEventListener("click", (event)=> {
     if (event.target.closest(".carousel__arrow_right")) {
       trnslt -= document.querySelector(".carousel__inner").offsetWidth;
     } else if (event.target.closest(".carousel__arrow_left")) {
       trnslt += document.querySelector(".carousel__inner").offsetWidth;
     }
-
     document.querySelector(".carousel__inner").style.transform = 'translateX(' + trnslt + 'px)';
 
     if (trnslt == document.querySelector(".carousel__inner").offsetWidth * -3) {
--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -14,5 +14,48 @@
  */
 export default class UserTable {
   constructor(rows) {
+    this.createElem();
+
+    this.createTable(rows);
+
+    this.createEvent();
+
+  }
+  createElem() {
+    this.elem = document.createElement("table");
+    this.elem.innerHTML = `
+    <thead>
+      <tr>
+        <th>Имя</th>
+        <th>Возраст</th>
+        <th>Зарплата</th>
+        <th>Город</th>
+        <th></th>
+      </tr>
+    </thead>
+    <tbody>
+    </tbody>`;
+  }
+
+  createTable(rows) {
+    for (let i = 0; i < rows.length; i++) {
+      let updatedRow = 
+      `<tr>
+        <td>${rows[i].name}</td>
+        <td>${rows[i].age}</td>
+        <td>${rows[i].salary}</td>
+        <td>${rows[i].city}</td>
+        <td><button>X</button></td>
+      </tr>`;
+      this.elem.querySelector("tbody").innerHTML += updatedRow;
+    }
+  }
+
+  createEvent() {
+    this.elem.querySelector("tbody").addEventListener("click", (event) => {
+      if (event.target.tagName == "BUTTON") {
+        event.target.closest("tr").remove();
+      }
+    });
   }
 }
