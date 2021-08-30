f7b8298519d77d95ca5feccb66b659816f395470
Artem Miroshnichenko <aimiroshnichenko@edu.hse.ru>
2021-08-30 10:20:31 +0300

    Учебный проект: Иконка корзины Учебный проект: Иконка корзины ошибка: иконка должна смещаться на 10px левее главного контейнера но мне кажется все и так работает

--- 8-module/1-task/index.js
+++ 8-module/1-task/index.js
@@ -21,6 +21,7 @@ export default class CartIcon {
           <span class="cart-icon__price">€${cart.getTotalPrice().toFixed(2)}</span>
         </div>`;
 
+      
       this.updatePosition();
 
       this.elem.classList.add('shake');
@@ -39,6 +40,33 @@ export default class CartIcon {
   }
 
   updatePosition() {
-    // ваш код ...
+    if (this.elem.classList.contains("cart-icon_visible") && document.body.clientWidth > 767) {
+      if (document.querySelector(".heading.logo").getBoundingClientRect().y < 0) {
+        this.elem.style.position = "fixed";
+        this.elem.style.zIndex = "1000";
+        this.elem.style.marginRight = "";
+        console.log(document.body.clientWidth - this.elem.getBoundingClientRect().right);
+        if (document.querySelector(".container").children[0].getBoundingClientRect().right + this.elem.getBoundingClientRect().width + 30 < document.body.clientWidth) {
+          this.elem.style.left = document.querySelector(".container").children[0].getBoundingClientRect().right + 20 + "px";
+        } else {
+          this.elem.style.left = document.body.clientWidth - this.elem.getBoundingClientRect().width - 10 + "px";
+        }
+      } else {
+        this.elem.style.position = "";
+        this.elem.style.zIndex = "";
+        this.elem.style.marginRight = "10px";
+        this.elem.style.left = "";
+      } 
+    } else {
+      this.elem.style.marginRight = "";
+    }
+
+    // console.log(document.querySelector(".container").children[0].getBoundingClientRect().right + this.elem.getBoundingClientRect.width + 10);
+
+    // if (document.querySelector(".container").children[0].getBoundingClientRect().right + this.elem.getBoundingClientRect().width + 30 < document.body.clientWidth) {
+    //   this.elem.style.left = document.querySelector(".container").children[0].getBoundingClientRect().right + 20 + "px";
+    // } else {
+    //   this.elem.style.left = document.body.clientWidth - this.elem.getBoundingClientRect().width - 10 + "px";
+    // }
   }
 }
