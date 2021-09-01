0188482bd65d8c2803d11cd9a9e051a0e3d305ac
Artem Miroshnichenko <aimiroshnichenko@edu.hse.ru>
2021-09-01 17:03:37 +0300

    Учебный проект: Корзина, часть 1

--- 8-module/3-task/index.js
+++ 8-module/3-task/index.js
@@ -1,28 +1,68 @@
 export default class Cart {
-  cartItems = []; // [product: {...}, count: N]
+  cartItems = []; // [product: {...}, count: 
 
   constructor(cartIcon) {
     this.cartIcon = cartIcon;
+    console.log("start");
   }
 
   addProduct(product) {
-    // ваш код
+    if (product != null) {
+      if (this.cartItems.length > 0) {
+        let end = this.cartItems.length;
+        let i = 0;
+        while (i < end) {
+          if (i != end && this.cartItems[i]["product"] == product) {
+            this.cartItems[i].count += 1;
+            break;
+          } else if (i + 1 == end) {
+            console.log("end");
+            this.cartItems.push({"product": product, "count": 1 });
+          }
+          i++;
+        }
+      } else {
+        this.cartItems.push({"product": product, "count": 1 });
+      }
+    }
+    this.onProductUpdate(this.cartItem); 
   }
 
   updateProductCount(productId, amount) {
-    // ваш код
+    for (let i of this.cartItems) {
+      if (i.product.id == productId) {
+        i.count += amount;
+        if (i.count == 0) {
+          this.cartItems.splice(this.cartItems.indexOf(i), 1);
+        }
+      }
+    }
+    console.log(this.cartItems.length);
+    this.onProductUpdate(this.cartItem);
   }
 
   isEmpty() {
-    // ваш код
+    if (this.cartItems.length > 0) {
+      return false;
+    } else {
+      return true;
+    }
   }
 
   getTotalCount() {
-    // ваш код
+    let count = 0;
+    for (let i of this.cartItems) {
+      count += i.count;
+    }
+    return count;
   }
 
   getTotalPrice() {
-    // ваш код
+    let priceAll = 0;
+    for (let i of this.cartItems) {
+      priceAll += i.count * i.product.price;
+    }
+    return priceAll;
   }
 
   onProductUpdate(cartItem) {
